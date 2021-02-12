f49df3239bea1d4297a4b178cebf847e47bd0ea9
Robert Grizzell <robert@grizzell.me>
2021-02-11 20:35:51 -0600

    Adding auto-update to dotfiles

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,6 +1,8 @@
 function fish_greeting
-	set -l changes (git -C ~/.dotfiles status -s | wc -l)
+	set -l changes (git -C $DOTFILES status -s | wc -l)
 	if test $changes -gt 0
 		echo "There are $changes dotfile changes pending."
+	else
+		git -C $DOTFILES pull -q
 	end
 end
