d9b1edd44e113b1179cf03c71733b6f9c7a81ecd
Robert Grizzell <robert@grizzell.me>
2021-08-25 00:06:32 -0500

    Additional GPG configuration

--- config/fish/conf.d/gpg.fish
+++ config/fish/conf.d/gpg.fish
@@ -1,2 +1,12 @@
+# GPG Key
 set -Ux KEYID F7F6C134977499ED
 set -Ux FINGERPRINT 29D193156276DFC178E8BDF3F7F6C134977499ED
+
+# GPG Agent
+if status is-interactive
+	if command -sq gpg-agent
+		set -x GPG_TTY (tty)
+		set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+		gpgconf --launch gpg-agent
+	end
+end
--- install_scripts/gpg.sh
+++ install_scripts/gpg.sh
@@ -9,8 +9,14 @@ shared_script_dir=$(dirname $0)
 
 # Set some values
 gpg_pkgs="gnupg2 gnupg-agent gnupg-utils scdaemon pcscd"
+yubikey_pkgs="yubikey-manager yubikey_personalization"
 
 if __package_check $gpg_pkgs; then
     echo "Install GPG packages"
     __package_install $gpg_pkgs
+fi
+
+if __package_check $yubikey_pkgs; then
+    echo "Install Yubikey packages"
+    __package_install $yubikey_pkgs
 fi
