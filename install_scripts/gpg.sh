2d1236e8ac9699d88b16c2d5694c8825f103f5ac
Robert Grizzell <robert@grizzell.me>
2021-08-24 19:30:33 -0500

    Adding GPG install script

--- install_scripts/gpg.sh
+++ install_scripts/gpg.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+# Install GPG packages needed for YubiKey support
 
-"gnupg2 gnupg-agent gnupg-curl scdaemon pcscd"
+# Import shared functions
+shared_script_dir=$(dirname $0)
+. $shared_script_dir/__package_check.sh
+. $shared_script_dir/__package_install.sh
+
+
+# Set some values
+gpg_pkgs="gnupg2 gnupg-agent gnupg-utils scdaemon pcscd"
+
+if __package_check $gpg_pkgs; then
+    echo "Install GPG packages"
+    __package_install $gpg_pkgs
+fi
