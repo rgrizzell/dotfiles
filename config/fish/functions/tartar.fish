418a958b742c181748311339d2779c2709632543
Robert Grizzell <robert@grizzell.me>
2021-09-20 14:11:03 -0500

    Adding support for XZ compression

--- config/fish/functions/tartar.fish
+++ config/fish/functions/tartar.fish
@@ -1,3 +1,14 @@
-function tartar --wraps='tar xzvf' --description='Unarchive a compressed tarball'
-  tar xzvf $argv
+function tartar --wraps='tar' --description='Unarchive a compressed tarball'
+	switch $argv
+		case "*.gz"
+			tar xzf $argv
+		case "*.tgz"
+			tar xzf $argv
+		case "*.xz"
+			tar xJf $argv
+		case "*.txz"
+			tar xJf $argv
+		case '*'
+			echo 'File unrecognized.'
+	end
 end
