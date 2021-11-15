8a41f31174df32ad79d0fe02a63cbd33d2df9693
Robert Grizzell <robert@grizzell.me>
2021-11-14 23:12:37 -0600

    Allow GPG key upload when private key is locked

--- config/fish/functions/gpg-copy-id.fish
+++ config/fish/functions/gpg-copy-id.fish
@@ -1,6 +1,6 @@
 function gpg-copy-id --wraps='ssh-copy-id -i $PUBKEY $argv' --description "Copies a GPG/SSH key to a remote SSH"
 	if command -sq gpg-agent
-		ssh-copy-id -i ~/.ssh/id_rsa_gpg.pub $argv
+		ssh-copy-id -f -i ~/.ssh/id_rsa_gpg.pub $argv
 	else
 		echo "GPG Agent not installed"
 	end
