69a72a26b2a95c18b6250a291aba17f0c8e2514c
Robert Grizzell <robert@grizzell.me>
2021-11-14 23:12:02 -0600

    Updating Fish Greeting with better Git

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,10 +1,21 @@
 function fish_greeting
-	set -l changes (git -C $DOTFILES status -s | wc -l)
+	function git
+		/usr/bin/git -C $DOTFILES $argv
+	end
+
+	set -l changes (git status -s | wc -l)
 	if test $changes -gt 0
 		echo "There are $changes dotfile changes pending."
 	else
 		echo "Checking for dotfile updates..."
-		git -C $DOTFILES pull -q
+		git fetch -q
+		set -l files (git diff --name-only HEAD origin/main)
+		if test $files -gt 0
+			git pull -q
+			$DOTFILES/install
+			read -n 1 -p '\nPress any key to continue'
+		end
 		clear
 	end
+	functions --erase git
 end
