b246a4d1b5d78e2b976097efcef1986b77464eff
Robert Grizzell <robert@grizzell.me>
2022-08-18 17:17:31 -0500

    Adding a few productivity plugins for Fish

--- install_scripts/fish_shell.sh
+++ install_scripts/fish_shell.sh
@@ -5,9 +5,24 @@
 shared_script_dir=$(dirname "$0")
 . "$shared_script_dir/__package_check.sh"
 
+# Set some values
+fish_pkgs="fish"
+extra_pkgs="grc"
+
 
 # Check if Fish is installed
-if ! __package_check "fish"; then
-    echo "Fish is not installed. Set the default shell by running the following:"
-    echo "  sudo apt install -y fish && chsh \$USER -s \$(command -v fish)"
+if __package_check $fish_pkgs; then
+    echo "Installing fish packages"
+    __package_install $fish_pkgs
+    fish=$(command -v fish)
+    if $fish; then
+        chsh $USER -s $fish
+    else
+        echo "Couldn't find fish for some reason. Default shell not set."
+    fi
+fi
+
+if __package_check $extra_pkgs; then
+    echo "Installing extra packages"
+    __package_install $extra_pkgs
 fi
