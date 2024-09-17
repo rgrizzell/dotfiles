e90724ec4d069c85f97865fc7bdba95ad14feb79
Robert Grizzell <robert@grizzell.me>
2024-09-17 10:01:54 -0500

    Updating PATH scripts

--- config/fish/functions/add-to-path.fish
+++ config/fish/functions/add-to-path.fish
@@ -1,5 +1,5 @@
 function add-to-path --description 'Persistently prepends paths to your PATH'
     contains -- $argv $fish_user_paths
-    or set -U fish_user_paths $fish_user_paths $argv
+    or set --global --export fish_user_paths $fish_user_paths $argv
     echo "Updated PATH: $PATH"
 end
